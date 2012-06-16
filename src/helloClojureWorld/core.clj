f2a1fb9365aee899da6d6d27de17ff3a54649929
Chaitanya Kuber <chaitanya.kuber@gmail.com>
2012-06-16 07:43:21 -0700

    added ability to receive post message and slurp data out of post body

--- src/helloClojureWorld/core.clj
+++ src/helloClojureWorld/core.clj
@@ -1,12 +1,14 @@
 (ns helloClojureWorld.core
-  (:use compojure.core)
+  (:use [compojure.core                 :only [defroutes GET POST]]
+	[ring.middleware.params         :only [wrap-params]]
+        [ring.middleware.keyword-params :only [wrap-keyword-params]])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]))
 
-(defroutes main-routes
+(defroutes routes
   (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (POST "/" req (println "request body:" (slurp (:body req))))
   (route/resources "/")
   (route/not-found "Page not found"))
 
-(def app
-  (handler/site main-routes))
+(def app (-> #'routes wrap-keyword-params wrap-params))
